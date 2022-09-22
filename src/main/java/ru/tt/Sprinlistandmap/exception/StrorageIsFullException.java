6f476a1fc2ea5f7b1b6260d163ad7fa4fc6f7179
jah <celt8@yandex.ru>
2022-09-22 10:49:52 +0500

    - hm ready

---
+++ src/main/java/ru/tt/Sprinlistandmap/exception/StrorageIsFullException.java
@@ -0,0 +1,11 @@
+package ru.tt.Sprinlistandmap.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.I_AM_A_TEAPOT)
+public class StrorageIsFullException extends RuntimeException{
+    public StrorageIsFullException(String message) {
+        super(message);
+    }
+}
