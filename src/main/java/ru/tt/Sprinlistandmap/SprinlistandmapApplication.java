69165d02914ea29a1e8c857d2e4e843651f933e8
jah <celt8@yandex.ru>
2022-09-22 10:19:49 +0500

    - init commit

---
+++ src/main/java/ru/tt/Sprinlistandmap/SprinlistandmapApplication.java
@@ -0,0 +1,13 @@
+package ru.tt.Sprinlistandmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinlistandmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinlistandmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/tt/Sprinlistandmap/SprinlistandmapApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tt.Sprinlistandmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinlistandmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
